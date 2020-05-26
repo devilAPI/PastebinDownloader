52940084b7cb3d4282ddc67c913b10c3a3296afa
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-26 12:06:14 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,10 +2,22 @@ import requests
 
 print("Welcome to Pastebin downloader!")
 print("")
-url = input("Url: ")
+print("Wich Paste Service are you using?")
+print("1 - pastebin.com")
+print("2 - hastebin.com")
+SERVICE = input("Service: ")
+print("Please enter your PasteID.")
+print("Your PasteID is the code after the / in your Link")
+PASTEID = input("PasteID: ")
 filename = input("Name of text file (e.g Downloaded pastebin): ")
 
-r = requests.get(pastebin_url)
+if SERVICE == "1":
+    SERVICEDOMAIN = "https://pastebin.com/raw/" + PASTEID
+elif SERVICE == "2":
+    SERVICEDOMAIN = "https://hastebin.com/raw/" + PASTEID
+
+
+r = requests.get(SERVICEDOMAIN)
 f = open(f'{filename}.txt', 'a+')
 f.write(r.text)
 print(f"Text file {filename}.txt has been made!")
