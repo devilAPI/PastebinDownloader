a5140e30b61c8fdad1d1165ba3d08e1aba3828df
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-26 13:49:42 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 import requests
+
 print("Welcome to Pastebin downloader!")
 print("")
 print("Wich Paste Service are you using?")
@@ -6,16 +7,20 @@ print("1 - pastebin.com")
 print("2 - hastebin.com")
 print("3 - github.com")
 SERVICE = input("Service: ")
+
 print("Please enter your PasteID.")
 print("Your PasteID is the code after the / in your Link")
 PASTEID = input("PasteID: ")
+
 filename = input("Name of text file (e.g Downloaded pastebin): ")
+
 if SERVICE == "1":
     SERVICEDOMAIN = "https://pastebin.com/raw/" + PASTEID
 elif SERVICE == "2":
     SERVICEDOMAIN = "https://hastebin.com/raw/" + PASTEID
 elif SERVICE == "3":
     SERVICEDOMAIN = "https://raw.githubusercontent.com/" + PASTEID
+
 r = requests.get(SERVICEDOMAIN)
 f = open(f'{filename}.txt', 'a+')
 f.write(r.text)
